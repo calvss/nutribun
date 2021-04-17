c68c833955923b88b2e5417da3e3fa7f06653ea9
Calvin Ng <calvinforum@rocketmail.com>
2021-04-17 18:34:56 +0800

    Added crafting recipe and tweaked values

--- src/main/java/com/calvss/nutribun/nutribun_item/Nutribun_Item.java
+++ src/main/java/com/calvss/nutribun/nutribun_item/Nutribun_Item.java
@@ -3,7 +3,6 @@ package com.calvss.nutribun.nutribun_item;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.Food;
-import net.minecraft.item.Foods;
 
 public class Nutribun_Item extends Item {
     public Nutribun_Item()
@@ -11,5 +10,5 @@ public class Nutribun_Item extends Item {
         super(new Item.Properties().stacksTo(64).tab(ItemGroup.TAB_MISC).food(nutribunFood) // the item will appear on the Miscellaneous tab in creative
         );
     }
-    private static final Food nutribunFood = (new Food.Builder()).nutrition(5).saturationMod(0.6F).build();
+    private static final Food nutribunFood = (new Food.Builder()).nutrition(8).saturationMod(0.7F).build();
 }
