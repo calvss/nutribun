b47709aa75a0cf745060dac30509f7e2f8bac906
Calvin Ng <calvin.alexander.ng@gmail.com>
2021-04-17 22:44:50 +0800

    Added Forge update checker
    Also removed unnecessary Diet integration functions.

--- src/main/java/com/calvss/nutribun/Nutribun.java
+++ src/main/java/com/calvss/nutribun/Nutribun.java
@@ -1,12 +1,7 @@
 package com.calvss.nutribun;
 
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.eventbus.api.Event;
 import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.fml.DistExecutor;
-import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod("nutribun")
@@ -15,15 +10,7 @@ public class Nutribun {
         // Get an instance of the mod event bus
         final IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
-        modEventBus.addListener(this::enqueue);
-
         registerCommonEvents(modEventBus);
-    }
-
-    private void enqueue(final InterModEnqueueEvent evt) {
-        if (ModList.get().isLoaded("diet")) {
-            //TODO: Diet integration
-        }
     }
 
     /**
